a03f936feef078931f1d0781b59b3f72046c82f7
hope <hope@yeahka.com>
2022-01-27 16:45:04 +0800

    add

--- go/main.go
+++ go/main.go
@@ -14,7 +14,6 @@ func main() {
 	// fmt.Println(problem.NumWaterBottles(9, 3))
 	// nums := []int{2,7,11,15}
 	// target := 9
-	num1 := []int{1,2}
-	num2 := []int{3,4}
-	fmt.Printf("res: %v\n", top100.FindMedianSortedArrays(num1, num2))
+	s := "babad"
+	fmt.Printf("res: %v\n", top100.LongestPalindrome(s))
 }
--- go/problem/top100/5_longestPalindrome.go
+++ go/problem/top100/5_longestPalindrome.go
@@ -19,12 +19,62 @@ import (
 输出："a"
 */
 
-func longestPalindrome(s string) string {
+func LongestPalindrome1(s string) string {
 	/*
-		
+		使用dp[i][j]=1代表从 i 到 j 为回文，那么如果s[i-1]==s[j+1]，dp[i-1][j+1] = 1
+		也即
+			dp[i][j]=dp[i+1][j-1](s[i]==s[j])；
+			dp[i][j]=0(s[i]!=s[j])
+			dp[i][i]=1
 	*/
 	var res string = ""
+	dp := make([][]bool, len(s))
+	for i := 0; i < len(s); i++ {
+		dp[i] = make([]bool, len(s))
+	}
 
-
+	for i := 0; i < len(s); i++ {
+		for j := 0; j <= i; j++ {
+			// 头尾相同，且中间也是回文
+			if s[i] == s[j] && (i-1 < j+1 || dp[i-1][j+1]) {
+				dp[i][j] = true
+			}
+			// 不断迭代最长的长度即可
+			if dp[i][j] && i-j+1 > len(res) {
+				res = s[j:i+1]
+				fmt.Printf("res: %s", res)
+			}
+		}
+	}
+	
 	return res
+}
+
+func LongestPalindrome(s string) string {
+	/*
+		中心扩展：字符串长度可以是奇数也可以是偶数
+
+	*/
+	n := len(s)
+	if n < 2 {
+		return s
+	}
+	start, end := 0, 0
+	for i := 0; i < n; {
+		l, r := i, i
+		//如果字符串相同则分别冲前一个和后一个开始回文
+		for r < n-1 && s[r] == s[r+1] {
+			r++
+		}
+		i = r + 1
+		for l > 0 && r < n-1 && s[l-1] == s[r+1] {
+			l--
+			r++
+		}
+		if end < r-l {
+			start = l
+			end = r - l
+		}
+	}
+	return s[start : start+end+1]
 }
