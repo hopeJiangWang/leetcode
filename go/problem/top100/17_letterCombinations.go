c589cd5971369efbc5a1f52c8efa93ee782c0dd2
hope <hope@yeahka.com>
2022-02-16 23:58:26 +0800

    17

--- go/problem/top100/17_letterCombinations.go
+++ go/problem/top100/17_letterCombinations.go
@@ -29,6 +29,7 @@ var phoneMap map[string]string = map[string]string{
 func LetterCombinations(digits string) []string {
 	res := new([]string)
 
+	// 如果字符串为空，直接返回空值
 	if len(digits) == 0 {
 		return *res
 	}
@@ -43,18 +44,20 @@ func LetterBacktracking(h int, res *[]string, digits, tmpStr string) {
 		回溯跳出的条件：按键使用完毕
 	*/
 	fmt.Printf("tmpStr: %v, res: %v \n", tmpStr, res)
-	if len(tmpStr) == len(digits) {
+	if h == len(digits) {
 		*res = append(*res, tmpStr)
-		return 
-	}
-	
-	// 依次选择本层数据，选好了就进入下一层递归
-	for _, v := range phoneMap[string(digits[h])] {
-		tmpStr += string(v)
-		fmt.Printf("nowStr: %v, h: %v \n", string(v), h)
-		LetterBacktracking(h+1, res, digits, tmpStr)
-		// 返回上一层，下一个数据
-		h = 0
-		tmpStr = tmpStr[0:len(tmpStr)-1]
+	} else {
+		// 依次选择本层数据，选好了就进入下一层递归
+		nowStr := phoneMap[string(digits[h])]
+		for i := 0; i < len(nowStr); i++ {
+			/*
+			tmpStr += string(nowStr[i])
+			// fmt.Printf("tmpStr: %v, nowStr: %v, h: %v \n", tmpStr, nowStr, h)
+			LetterBacktracking(h+1, res, digits, tmpStr)
+			// 返回上一层，下一个数据
+			tmpStr = tmpStr[0:len(tmpStr)-1]
+			*/
+			LetterBacktracking(h+1, res, digits, tmpStr + string(nowStr[i]))
+		}
 	}
 }
