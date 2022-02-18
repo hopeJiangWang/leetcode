bd4bdf0d5e115e64fe20b77a09e0beaea5e9ae15
hope <hope@yeahka.com>
2022-02-18 18:35:51 +0800

    2022/02/18

---
+++ go/problem/top100/34_searchRange.go
@@ -0,0 +1,16 @@
+package top100
+
+/*
+输入：nums = [5,7,7,8,8,10], target = 8
+输出：[3,4]
+
+输入：nums = [5,7,7,8,8,10], target = 6
+输出：[-1,-1]
+
+输入：nums = [], target = 0
+输出：[-1,-1]
+*/
+
+func searchRange(nums []int, target int) []int {
+
+}
